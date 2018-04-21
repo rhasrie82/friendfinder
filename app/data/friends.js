3463345868ea9c2e9d13cd3b551808c0d049c73b
rhasrie82 <33564425+rhasrie82@users.noreply.github.com>
2018-04-21 01:38:32 -0400

    First commit

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+var friendsData = [
+    {
+      name: "Mya Harrison",
+      photo: "https://cps-static.rovicorp.com/3/JPG_500/MI0001/368/MI0001368303.jpg?partner=allrovi.com",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Beyonce Knowles",
+      photo:"http://scstylecaster.files.wordpress.com/2013/08/beyonce-eyelashes-instagram.jpg?w=612&h=612",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Rita Ora",
+      photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeBjiKEdx4iE7w2F8FsaXDf1SgLs7jiCHlDtIYR0d_9jlL1qYy",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Chris Brown",
+      photo:"http://cdn.gospelherald.com/data/images/full/11562/chris-brown.jpg",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Michael Jackson",
+      photo:"https://img.discogs.com/CeQN1dydvKgeCV4mc7aOBnXTpOk=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-15885-1506759724-8583.jpeg.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Camilla Cabello",
+      photo:"http://cdn02.cdn.justjaredjr.com/wp-content/uploads/headlines/2018/01/camila-cabello-loreal-elvive-commercial.jpg",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "The Weeknd",
+      photo:"https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/04/15/11/the-weeknd.jpg",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
+  
